66b97ad8cc39454b36b01e484a1599c92d26bb23
michaelmnemonic <michaelmnwmonic@posteo.eu>
2024-12-23 09:09:05 +0100

    Configuring flatpak does not work this way

--- _shared/tmp/00_customize.sh
+++ _shared/tmp/00_customize.sh
@@ -38,13 +38,6 @@ dnf install -y                                                                  
 # Use luks partition for home of user "MAIK"
 sed -i '/<!-- Volume definitions -->/a <volume user="maik" fstype="crypt" path="/dev/disk/by-partlabel/MAIK" mountpoint="~" options="crypto_name=MAIK,fstype=ext4" />' /etc/security/pam_mount.conf.xml
 
-# Add flathub
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
-# Disable fedora flatpaks
-flatpak remote-modify --disable fedora
-flatpak remote-modify --disable fedora-testing
-
 # Enable automatic updates
 systemctl enable bootc-fetch-apply-updates.timer
 
