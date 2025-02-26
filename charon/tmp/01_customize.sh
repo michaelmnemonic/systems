3552391103b601655f6e4b6b07a7d4b21db976a1
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-02-26 09:02:44 +0100

    Rebuild initrd

--- charon/tmp/01_customize.sh
+++ charon/tmp/01_customize.sh
@@ -2,3 +2,6 @@
 
 # Set default devicetree in grub
 echo "GRUB_DEFAULT_DTB=/dtb/qcom/sc8280xp-lenovo-thinkpad-x13s.dtb" >> /etc/default/grub
+
+# Rebuild initrd
+kver=$(cd /usr/lib/modules && echo *); dracut -vf /usr/lib/modules/$kver/initramfs.img $kver
