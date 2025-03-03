e4dfef199e8cee10c4c1ef7bc8f56f85306b32df
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-03-03 20:06:46 +0100

    Enable luks encrypted home

--- _shared/tmp/00_customize.sh
+++ _shared/tmp/00_customize.sh
@@ -41,6 +41,9 @@ dnf install -y                                                                  
 # Use luks partition for home of user "MAIK"
 sed -i '/<!-- Volume definitions -->/a <volume user="maik" fstype="crypt" path="/dev/disk/by-partlabel/MAIK" mountpoint="~" options="crypto_name=MAIK,fstype=ext4" />' /etc/security/pam_mount.conf.xml
 
+# Switch to luks encrypted home
+authselect select custom/lukshome
+
 # Enable automatic updates
 systemctl enable bootc-fetch-apply-updates.timer
 
