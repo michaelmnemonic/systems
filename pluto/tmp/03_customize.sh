31cdf596aeb2bc86ebfa4930ccb09b067d973c71
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-04-24 11:35:09 +0200

    Make sure that kde desktop can be removed

--- pluto/tmp/03_customize.sh
+++ pluto/tmp/03_customize.sh
@@ -2,8 +2,32 @@
 
 # This script installs a session that uses the niri compositor
 
+# install niri session
 dnf install -y \
   niri         \
   waybar       \
   alacritty    \
   pavucontrol
+
+# Add lm-sensor (needed for fan2go)
+dnf install -y                                                                                         \
+  lm_sensors                                                                                           \
+  lm_sensors-libs
+
+# Add gaming appllications
+dnf install -y                                                                                         \
+  --setopt=excludepkgs=nvtop                                                                           \
+  gamescope                                                                                            \
+  mangohud                                                                                             \
+  steam
+
+# Add wine
+dnf install -y                                                                                        \
+ wine-arial-fonts                                                                                     \
+ wine-common                                                                                          \
+ wine-core                                                                                            \
+ wine-ldap                                                                                            \
+ wine-mono                                                                                            \
+ wine-pulseaudio                                                                                      \
+ wine-times-new-roman-fonts                                                                           \
+ wine-times-new-roman-fonts-system
